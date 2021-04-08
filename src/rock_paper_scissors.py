32cfc022612f8f8cb471a731a7c53a367ea12491
MistaRae <itsamistarae@gmail.com>
2021-04-08 15:10:43 +0100

    testing for equality defined and working

--- src/rock_paper_scissors.py
+++ src/rock_paper_scissors.py
@@ -1,2 +1,8 @@
 # import unittest ???
 from src.player_class import Player
+
+
+def play_game(player1, player2):
+    if player1.gesture == player2.gesture:
+        return True
+    return False
--- test/test_rock_paper_scissors.py
+++ test/test_rock_paper_scissors.py
@@ -6,6 +6,9 @@ from src.rock_paper_scissors import *
 class TestRockPaperScissors(unittest.TestCase):
 
     def setUp(self):
-        self.player_1 = Player("lina", "rock")
-        self.player_2 = Player("mark", "scissors")
+        self.player_1 = Player("Lina", "rock")
+        self.player_2 = Player("Mark", "scissors")
         self.player_3 = Player("Colin", "paper")
+
+    def test_if_gesture1_equals_gesture2(self):
+        self.assertEqual(False, play_game(self.player_1, self.player_2))
